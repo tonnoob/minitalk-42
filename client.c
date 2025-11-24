a796e4a88870d6301bb6fd0145ca85143ab2e5c2
Tonnoob <ottonsouzanto@gmail.com>
2025-11-24 01:24:50 -0300

    Refazendo o ACK do serve e reformulando um pouco o código

--- client.c
+++ client.c
@@ -6,16 +6,31 @@
 /*   By: otton-sousa <otton-sousa@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 18:22:11 by osousa-d          #+#    #+#             */
-/*   Updated: 2025/11/20 18:47:10 by otton-sousa      ###   ########.fr       */
+/*   Updated: 2025/11/24 01:19:29 by otton-sousa      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+// static void handler_client(int sig, siginfo_t *info, void *context)
+// {
+// 	(void)info;
+// 	(void)context;
+// 	(void)sig;
+// }
+
+static void	ack_end(int sig)
+{
+	if (sig == SIGUSR2)
+		write(1, "OK!", 3);
+	else if (sig == SIGUSR1)
+
+}
+
 void	send_message(unsigned char byte, __pid_t server_pid)
 {
-	int	bit;	
-
+	int	bit;
+	
 	bit = 0;
 	while (bit <= 7)
 	{
@@ -23,36 +38,29 @@ void	send_message(unsigned char byte, __pid_t server_pid)
 			kill(server_pid, SIGUSR1);
 		else
 			kill(server_pid, SIGUSR2);
+		usleep(2000);
 		bit++;
-		pause();
 	}
-}
-
-static void	client_handler(int sig)
-{
-	(void)sig;
 }
 
 int	main(int argc, char **argv)
 {
-	__pid_t				server_pid;
-	int					i;
+	__pid_t	server_pid;
+	int		i;
 
-
-	signal(SIGUSR1, client_handler);
-	if (argc != 3)
+	if (argc == 3)
 	{
+		server_pid = ft_atoi(argv[1]);
+		signal(SIGUSR1, send_message);
+		signal(SIGUSR2, ack_end);
+		i = 0;
+		while (argv[2][i])
+		{
+			send_message((unsigned char)argv[2][i], server_pid);
+			i++;
+		}
+	}
+	else
 		ft_printf("Invalid argument\n");
 		exit(EXIT_FAILURE);
-	}
-	server_pid = ft_atoi(argv[1]);
-	i = 0;
-	while (argv[2][i])
-	{
-		send_message((unsigned char)argv[2][i], server_pid);
-		i++;
-	}
-	send_message('\0', server_pid);
-	pause();
-	return (0);
 }
--- server.c
+++ server.c
@@ -6,57 +6,52 @@
 /*   By: otton-sousa <otton-sousa@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 18:32:10 by osousa-d          #+#    #+#             */
-/*   Updated: 2025/11/20 18:28:27 by otton-sousa      ###   ########.fr       */
+/*   Updated: 2025/11/23 22:28:16 by otton-sousa      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-// static void	accumulate_buffer(unsigned char byte)
-// {
-// 	static int		i;
-// 	static char		buffer[100000];
-
-// 	if (byte != 0)
-// 	{
-// 		buffer[i] = byte;
-// 		i++;
-// 	}
-// 	if (byte == 0)
-// 	{
-// 		buffer[i] = '\0';
-// 		ft_printf("%s\n", buffer);
-// 		i = 0;
-// 	}
-// }
-
-static void	handler_signal(int sig)
+static void handler_signal(int sig, siginfo_t *info, void *context)
 {
 	static int				bit;
 	static unsigned char	byte;
+	static __pid_t			pid_client;
 
+	if (pid_client == 0)
+		pid_client = info->si_pid;
 	if (sig == SIGUSR1)
 		byte |= (1 << bit);
 	bit++;
 	if (bit == 8)
 	{
-		write(1, &byte, 1);
-		if (byte == 0)
-		{	
-			ft_printf("\n");
-		}
 		bit = 0;
-		byte = 0;
+		if (byte == '\0')
+		{
+			kill(pid_client, SIGUSR2);
+			pid_client = 0;
+			return ;
+		}
+		else
+		{
+			write(1, &byte, 1);
+			byte = 0;
+		}
 	}
+	kill(pid_client, SIGUSR1);
 }
 
-int	main(void)
+int main(void)
 {
+	struct sigaction sa;
+
 	ft_printf("%d\n", getpid());
-	signal(SIGUSR1, handler_signal);
-	signal(SIGUSR2, handler_signal);
+	sa.sa_sigaction = handler_signal;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+
 	while (1)
-	{
 		pause();
-	}
 }
