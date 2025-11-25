31929e73a231c360cfc3df7ff82232b37459675d
tonnoob <ottonsouzanto@gmail.com>
2025-11-25 19:16:56 -0300

    Create global varable

--- client.c
+++ client.c
@@ -12,55 +12,65 @@
 
 #include "minitalk.h"
 
-// static void handler_client(int sig, siginfo_t *info, void *context)
-// {
-// 	(void)info;
-// 	(void)context;
-// 	(void)sig;
-// }
+volatile sig_atomic_t	g_ack = 1;
 
-static void	ack_end(int sig)
+static void	ack_client(int sig)
 {
-	if (sig == SIGUSR2)
-		write(1, "OK!", 3);
-	else if (sig == SIGUSR1)
-
+	if (sig == SIGUSR1)
+		g_ack = 0;
+	else if (sig == SIGUSR2)
+	{
+		ft_printf("✅ FINISH!\n");
+		exit (0);
+	}
 }
 
 void	send_message(unsigned char byte, __pid_t server_pid)
 {
 	int	bit;
-	
+
 	bit = 0;
 	while (bit <= 7)
 	{
+		g_ack = 1;
 		if ((byte >> bit) & 1)
 			kill(server_pid, SIGUSR1);
 		else
 			kill(server_pid, SIGUSR2);
-		usleep(2000);
+		while (g_ack)
+			pause();
 		bit++;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	__pid_t	server_pid;
-	int		i;
+	__pid_t				server_pid;
+	struct sigaction	sa;
+	int					i;
 
-	if (argc == 3)
+	sa.sa_handler = ack_client;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	if (argc != 3)
 	{
-		server_pid = ft_atoi(argv[1]);
-		signal(SIGUSR1, send_message);
-		signal(SIGUSR2, ack_end);
-		i = 0;
-		while (argv[2][i])
-		{
-			send_message((unsigned char)argv[2][i], server_pid);
-			i++;
-		}
-	}
-	else
 		ft_printf("Invalid argument\n");
 		exit(EXIT_FAILURE);
+	}
+	server_pid = (__pid_t)ft_atoi(argv[1]);
+	if (server_pid <= 0)
+	{
+		printf("Invalid PID\n");
+		return (EXIT_FAILURE);
+	}
+	i = 0;
+	while (argv[2][i])
+	{
+		send_message((unsigned char)argv[2][i], server_pid);
+		i++;
+	}
+	send_message((unsigned char)argv[2][i], server_pid);
+	return (EXIT_SUCCESS);
 }
--- server.c
+++ server.c
@@ -12,14 +12,14 @@
 
 #include "minitalk.h"
 
-static void handler_signal(int sig, siginfo_t *info, void *context)
+static void	handler_signal(int sig, siginfo_t *info, void *context)
 {
 	static int				bit;
 	static unsigned char	byte;
 	static __pid_t			pid_client;
 
-	if (pid_client == 0)
-		pid_client = info->si_pid;
+	(void)context;
+	pid_client = info->si_pid;
 	if (sig == SIGUSR1)
 		byte |= (1 << bit);
 	bit++;
@@ -27,31 +27,25 @@ static void handler_signal(int sig, siginfo_t *info, void *context)
 	{
 		bit = 0;
 		if (byte == '\0')
-		{
 			kill(pid_client, SIGUSR2);
-			pid_client = 0;
-			return ;
-		}
 		else
-		{
 			write(1, &byte, 1);
-			byte = 0;
-		}
+		byte = 0;
 	}
 	kill(pid_client, SIGUSR1);
 }
 
-int main(void)
+int	main(void)
 {
-	struct sigaction sa;
+	struct sigaction	sa;
 
-	ft_printf("%d\n", getpid());
 	sa.sa_sigaction = handler_signal;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-
+	ft_printf("PID Server: %d\n", getpid());
 	while (1)
 		pause();
+	return (EXIT_SUCCESS);
 }
